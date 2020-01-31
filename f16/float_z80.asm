ee34d8c2f41f947044f7ac41e9a5f3d3db1f5c40
Artyom Beilis <artyomtnk@yahoo.com>
2020-02-01 00:05:53 +0200

    Improvements of mpl performance

--- f16/float_z80.asm
+++ f16/float_z80.asm
@@ -263,8 +263,6 @@ fast_sub_shift_add_sign_and_ret:
     ld h,a
     ret
 
-
-
 handle_nan:
     ld hl,0x7FFF
     ret
@@ -272,219 +270,36 @@ handle_nan:
 
 
 
-_f16_sub_prev:
-    push ix
-    ld ix,0
-    add ix,sp
- 	ld	l,(ix+4)
-	ld	h,(ix+5)
-	ld	e,(ix+6)
-    ld a,0x80
-	xor (ix+7)
-    ld d,a
-    jr fadd_entry_after_stack_prepare
-
-
-
-_f16_add_pos_he_gte_de_:
-    ld a,0x7c
-    and d
-    ld b,a
-    ld a,0x7c
-    and h
-    cp b ; compare (m1*4) with (m2*4)
-    
-
-_f16_add_prev:
-    push ix
-    ld ix,0
-    add ix,sp
- 	ld	l,(ix+4)
-	ld	h,(ix+5)
-	ld	e,(ix+6)
-	ld	d,(ix+7)
-    jr fadd_entry_after_stack_prepare
-
-_fsub_hl_de:
-    and a,0x80
-    xor d
-    ld d,a
-
-_fadd_hl_de:
-    push ix
-    ld ix,0
-    add ix,sp
-fadd_entry_after_stack_prepare:
-    push af
-    push af
-    call check_invalid
-    jp z,return_nan
-    ld a,h
-    ld b,0x80 ; sign mask
-    and b
-    ld (ix + sign),a  ; store sign(A)
-    ld c,a ; save sign to c
-    xor d
-    and b
-    ld (ix + op_sub),a   ; op_sub = sign(A) ^ sign(B)
-    ld b,a ; save sub to b
-
-    res 7,h         ; we don't keep sing any more
-    res 7,d         ; A=abs(A) , B=abs(B)
-
-    ld a,l          ; check if A-B>=0 no swap
-    sub e
-    ld a,h
-    sbc d
-    jr nc,no_swap
-    ex de,hl        ; swap(A,B)
-    ld a,b   ; if op_sub -> sign = -sign
-    xor c
-    ld (ix+sign),a
-no_swap:
-    call calc_ax_bx_mantissa_on_abs
-    ld (ix+exp),c          ; exp = EXP(A)
-    sub b                  ; b=ax - bx
-    ld b,a
-    ld a,(ix+op_sub)       ; if op_sub - go to substr
-    and a
-    jr nz,substruction
-    ld a,b              ; if ax - bx == 0 no shoft
-    and a
-    jr z,no_diff_add    ; 
-shift_de_add:
-    sra d
-    rr e
-    djnz shift_de_add    ; de>> b
-no_diff_add:
-    add hl,de           ; hl = m1+m2
-    ld a,(ix+exp)          ; 
-    bit 3,h             ; hl & 2048 exp++, hl >>=1
-    jr z,fix_subnormal_add_exp
-    inc a
-    sra h
-    rr l
-    jr final_combine
-fix_subnormal_add_exp:  ; if exp==0 and hl & 1024 exp++
-    and a
-    jr nz,final_combine
-    bit 2,h
-    jr z,final_combine
-    inc a
-    jr final_combine
-    
-substruction:
-    add hl,hl   ; m1<<=1, m2<<=1
-    ex de,hl
-    add hl,hl
-    ex de,hl
-    ld a,b
-    and a
-    jr z,no_diff_sub
-shift_de_sub:   ; m2 >>= diff
-    sra d
-    rr e
-    djnz shift_de_sub
-no_diff_sub:
-    ld a,l
-    sub e
-    ld l,a
-    ld a,h
-    sbc d   ; m1 = m1-m2
-    ld h,a
-    or l
-    jp z,exit_point ; if diff = 0 return 0
-    ld a,(ix+exp)              ; while exp > 0 and bit 2048 not set
-next_shift_sub:
-    bit 3,h
-    jr nz,sub_shift_done
-    and a,a
-    jr z,sub_shift_done
-    dec a                       ; exp --
-    jr z,sub_shift_done         ; if exp==0 break
-    add hl,hl                   ; m1<<=1
-    jr next_shift_sub       ; end while
-sub_shift_done:
-    sra h   ; m1>>=1
-    rr l
-final_combine:              
-    cp 31
-    jr nc, return_inf
-    add a
-    add a 
-    or a,(ix+sign)
-    res 2,h ; remove hidden bit
-    or h
-    ld h,a
-    or 0x7F         ; reset -0 to normal 0
-    or l
-    jr nz,exit_point
-    ld hl,0
-exit_point:
-    pop af
-    pop af
-    pop ix
-    ret
-  
-return_inf:
-    ld a,(ix+sign)
-    or 0x7C
-    ld h,a
-    ld l,0
-    jr exit_point
-return_nan:
-    ld hl,0x7FFF
-    jr exit_point
-
-check_invalid:
-    ; check if hl or de is inf/nan, returns flag z if is invalid
-    ld c,0x7C       ; if exp(A) == 31 goto return nan
-    ld a,h
-    and c
-    cp c
-    ret z
-    ld a,d          ; if exp(B) == 31 goto return nan
-    and c
-    cp c
-    ret
-
 _f16_mul:
-    push ix
-    ld ix,0
-    add ix,sp
- 	ld	l,(ix+4)
-	ld	h,(ix+5)
-	ld	e,(ix+6)
-	ld	d,(ix+7)
-    jr fmul_entry_after_stack_prepare
-     
+    pop bc
+    pop hl
+    pop de
+    push de
+    push hl
+    push bc
 _fmul_hl_de:
-    push ix
-    ld ix,0
-    add ix,sp
-
-fmul_entry_after_stack_prepare:
-    push af
-    push af
-    call check_invalid
-    jp z,return_nan
     ld a,h
     xor d
     and 0x80
-    ld (ix + sign),a  ; sign = sig(A) ^ sig(B)
+    ex af,af'
     call calc_ax_bx_mantissa
+    jr z,handle_nan
     add b   ; new_exp = ax + bx - 15
     sub 15
-    ld (ix+exp),a 
+    exx 
+    ld c,a ; save exp in bc'
+    exx
     ld a,h
     or l
-    jp z,exit_point
+    ret z
     ex de,hl
     ld a,h
     or l
-    jp z,exit_point
+    ret z
     call mpl_11_bit  ; de/hl = m1*m2
-    ld a,(ix+exp)
+    exx
+    ld a,c
+    exx 
     bit 5,e                      ; if v >=2048: m>>11, exp++
     jr nz,fmul_exp_inc_shift_11
     bit 4,e
@@ -537,7 +352,21 @@ fmul_denorm_shift:
     rr h
     rr l
     djnz fmul_denorm_shift
-    jp final_combine
+final_combine:              
+    cp 31
+    jr nc, handle_nan
+    add a
+    add a 
+    res 2,h ; remove hidden bit
+    or h
+    ld h,a
+    or 0x7F         ; reset -0 to normal 0
+    or l
+    ret z
+    ex af,af' ; restore sign
+    or h
+    ld h,a
+    ret
     
 
     ; input hl,de
@@ -547,16 +376,20 @@ fmul_denorm_shift:
     ;   c = exp(hl)
     ;   hl = mantissa(hl)
     ;   de = mantissa(de)
+    ;   z flag one of the numbers is inf/nan
+    ;   no calcs done
 
 
 calc_ax_bx_mantissa:
     res 7,h
     res 7,d
-calc_ax_bx_mantissa_on_abs:
+    ld c,0x7c
     ld a,d
-    and 0x7c
+    and c
     jr z,bx_is_zero
-    rra  
+    cp c
+    ret z
+    rrca  
     rra 
     ld b,a ; b=bx
     ld a,3
@@ -569,9 +402,11 @@ bx_is_zero:
 bx_not_zero:
     ; b is bx, de=mantissa(de)
     ld a,h
-    and 0x7c
+    and c
     jr z,ax_is_zero 
-    rra
+    cp c
+    ret z
+    rrca
     rra 
     ld c,a  ; c=exp
     ld a,3
@@ -582,6 +417,7 @@ bx_not_zero:
     ret
 ax_is_zero:
     ld a,1 ; hl is already ok a=ax
+    and a ; reset z flag
     ld c,0 ; c=exp(hl)
     ret 
 
